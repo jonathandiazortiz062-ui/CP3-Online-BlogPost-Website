1c3d60a48356da2506e65d718756744ed8c5910c
Jonathan <nathan.diaz062@gmail.com>
2026-06-17 10:44:56 -0400

    Main Structure - Done

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
